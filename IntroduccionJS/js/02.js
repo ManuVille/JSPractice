3f5922586a144e15304e280d5bcc76dbcfa7b2ce
Emmanuel Villegas <EmmaVilleAgui@gmail.com>
2023-03-16 17:20:19 -0600

    r2

---
+++ IntroduccionJS/js/02.js
@@ -0,0 +1,13 @@
+//variables con let
+
+let producto = 'audifonos gamer';
+
+let disponible;
+
+disponible = true;
+
+//estilo de variables 
+let nombre_producto = 'monitor hd';
+let nombreProducto = 'monitor hd';
+let NombreProducto = 'monitor hd';
+let nombreproducto = 'monitor hd';
---
+++ IntroduccionJS/js/05.js
@@ -0,0 +1,8 @@
+
+const nombre = "Emmanuel";
+const numero1 = 100;
+const numero2 = 200;
+
+console.log(nombre);
+console.log(numero1 + numero2);
+console.log(numero2);
